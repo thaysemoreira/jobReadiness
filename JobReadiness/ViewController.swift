563350305b7d6e13af75e31f30344186c1a937f0
Thayse Moreira <thayse.moreira@mercadolivre.com>
2022-06-29 11:23:36 -0300

    first commit

---
+++ JobReadiness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobReadiness
+//
+//  Created by Thayse Bianca Amaral De Carvalho Moreira on 29/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
